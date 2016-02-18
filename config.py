d3fea922aa60f2b4081a0b09d05b29461f5a6e4e
Chris Shepard <csshepard@gmail.com>
2016-02-18 14:27:24 -0500

    Working Prototype using GUnicorn

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = "change-me"
+    SECRET_KEY = "A\xd5p\xa3\xda\xef\x9esL\x0e\xb1\xe0\x91\xba\xc2\x90\xbeb\xb4\x84\xb0jA\xdd"
 
 class ProductionConfig(Config):
     DEBUG = False
