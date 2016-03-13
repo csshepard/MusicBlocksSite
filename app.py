f9b50fcc8f6fb066e2cc1ffe1b8e5f60abacbb16
Chris Shepard <csshepard@gmail.com>
2016-03-13 16:17:41 -0400

    prevents deleting identical songs

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, g
 from werkzeug.utils import secure_filename
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
@@ -9,7 +9,7 @@ from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 
-PATH = os.path.dirname(os.path.realpath(__file__))
+PATH = os.path.dirname(__file__)
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
@@ -19,7 +19,7 @@ Bootstrap(app)
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect('/home/Chris/Projects/MusicBlocksSite/MusicBlocks.db',
+        db = g._database = sqlite3.connect(PATH + 'MusicBlocks.db',
                                            detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)
         db.row_factory = sqlite3.Row
     return db
@@ -33,7 +33,7 @@ def close_connection(exception):
 
 
 class ChangeSong(Form):
-    file = FileField('File', validators=[FileRequired(), FileAllowed(['mp3'],'Mp3s only')])
+    file = FileField('File', validators=[FileRequired(), FileAllowed(['mp3'], 'Mp3s only')])
     block_number = SelectField('Block #', validators=[DataRequired()], coerce=int)
     song_title = StringField('Song Title', validators=[DataRequired()])
     submit = SubmitField('Submit')
@@ -43,21 +43,24 @@ class ChangeSong(Form):
 def index():
     form = ChangeSong()
     cursor = get_db().cursor()
+    blocks = cursor.execute('SELECT * FROM block_table').fetchall()
+    form.block_number.choices = [(block['block_number'], block['block_number']) for block in blocks]
     if form.validate_on_submit():
         song = secure_filename(form.file.data.filename)
         cursor.execute('SELECT file_name FROM song_table WHERE block_number=?', str(form.block_number.data))
         old_file = cursor.fetchone()['file_name']
-        form.file.data.save('Music/' + song)
+        form.file.data.save(PATH + 'Music/%s' % song)
         cursor.execute('UPDATE song_table SET song_name=?, file_name=? WHERE block_number=?', (form.song_title.data,
                                                                                                song, str(form.block_number.data)))
         get_db().commit()
         try:
-            os.remove(PATH+'/Music/%s' % old_file)
+            if old_file != song:
+                os.remove(PATH + 'Music/%s' % old_file)
         except OSError:
             pass
     cursor.execute('SELECT block_number, song_table.song_name, count FROM song_table LEFT OUTER JOIN (SELECT song_name AS sn, count(*) AS count FROM play_history_table GROUP BY song_name) ON song_name = sn''')
     blocks = cursor.fetchall()
-    cursor.execute('SELECT * FROM play_history_table')
+    cursor.execute('SELECT time_played, song_table.song_name, block_number FROM play_history_table INNER JOIN song_table ON song_table.song_name=play_history_table.song_name')
     history = cursor.fetchmany(10)
     return render_template('index.html', cs_form=form, blocks=blocks, history=history)
 
